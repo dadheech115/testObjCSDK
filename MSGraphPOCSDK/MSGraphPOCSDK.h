33f1b4a87203778ec58aef044980bac4245a916c
Vikas Dadheech <vidadhee@microsoft.com>
2018-07-25 15:42:37 +0530

    Initial Commit

---
+++ MSGraphPOCSDK/MSGraphPOCSDK.h
@@ -0,0 +1,19 @@
+//
+//  MSGraphPOCSDK.h
+//  MSGraphPOCSDK
+//
+//  Created by Vikas Dadheech on 25/07/18.
+//  Copyright © 2018 Vikas Dadheech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSGraphPOCSDK.
+FOUNDATION_EXPORT double MSGraphPOCSDKVersionNumber;
+
+//! Project version string for MSGraphPOCSDK.
+FOUNDATION_EXPORT const unsigned char MSGraphPOCSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSGraphPOCSDK/PublicHeader.h>
+
+
